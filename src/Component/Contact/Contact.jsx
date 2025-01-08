150be4a70b72b18b5a883549766d85497f1c96f9
Fumika Mikami <fumicha.3fan1@gmail.com>
2025-01-08 15:46:59 +0900

    up

--- src/Component/Contact/Contact.jsx
+++ src/Component/Contact/Contact.jsx
@@ -6,6 +6,31 @@ import location_icon from '../../assets/location_icon.svg'
 
 
 const Contact = () => {
+
+    const onSubmit = async (event) => {
+        event.preventDefault();
+        const formData = new FormData(event.target);
+    
+        formData.append("access_key", "b5b534d4-e465-40b5-8521-471e68e0fb39");
+    
+        const object = Object.fromEntries(formData);
+        const json = JSON.stringify(object);
+    
+        const res = await fetch("https://api.web3forms.com/submit", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json"
+          },
+          body: json
+        }).then((res) => res.json());
+    
+        if (res.success) {
+          alert(res.message);
+        }
+  };
+
+
   return (
     <div id='contact' className='contact'>
         <div className="contact-title">
@@ -26,7 +51,7 @@ const Contact = () => {
                     </div>
                 </div>
             </div>
-            <form className='contact-right'>
+            <form onSubmit={onSubmit} className='contact-right'>
                 <label htmlFor="">Your Name</label>
                 <input type="text" placeholder='ENter your name' name='name'/>
                 <label htmlFor="">Your Email</label>
